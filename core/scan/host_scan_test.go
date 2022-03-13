6228d144d1685c6e60c8ab43a9917cef36179a94
RaidriarB <1085455474@qq.com>
2022-03-13 20:49:24 +0800

    将原先的三个包整合到一个scan包中

---
+++ core/scan/host_scan_test.go
@@ -0,0 +1,33 @@
+package scan
+
+import (
+	"fmt"
+	"testing"
+
+	"myscanner/core/types"
+)
+
+func TestLoadTargets(t *testing.T) {
+	// TODO: 编写测试
+
+	fmt.Printf("\n%#v\n\n", LoadTargets())
+}
+
+func TestScanTargets(t *testing.T) {
+	targets := types.Targets{
+		IPAddrs: []string{
+			"10.2.1.3", "10.2.1.4", "10.2.1.6", "10.2.1.7", "10.2.1.10", "10.2.1.13",
+			"10.2.3.4", "10.2.3.5", "10.2.3.8", "10.2.3.9",
+			"10.2.4.12", "10.2.4.15",
+		},
+		IPRanges: []types.IPRange{
+			{Start: "10.5.254.252", End: "10.5.255.6"},
+			{Start: "10.6.7.0", End: "10.6.7.7"},
+		},
+	}
+
+	var alivehosts []string = ScanTargets(targets)
+	fmt.Println("存活的主机如下：")
+	fmt.Println(alivehosts)
+
+}
